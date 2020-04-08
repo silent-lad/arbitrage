841ce2384fece81b732fa5220ecf4ba32851b70c
DIVYANSH TRIPATHI <divyanshtripathi1998@gmail.com>
2020-04-08 17:03:29 +0530

    taking to gcp

--- app.js
+++ app.js
@@ -1,35 +1,32 @@
 // import axios from "axios";
 
 const axios = require("axios");
+const logger = require("morgan");
+const express = require("express");
 
-axios.get("https://www.bitstamp.net/api/ticker/").then((res) => {
-  console.log(res.data);
+const app = express();
+
+app.use(logger("dev"));
+app.use(express.static("public"));
+
+app.get("/api", (req, res) => {
+  axios
+    .all([
+      axios.get("https://www.bitstamp.net/api/ticker/"),
+      axios.get("https://www.freeforexapi.com/api/live?pairs=USDZAR"),
+      axios.get("https://api.mybitx.com/api/1/ticker?pair=XBTZAR"),
+    ])
+    .then((resArr) => {
+      var response = {
+        ...resArr[0].data,
+        ...resArr[1].data,
+        ...resArr[2].data,
+      };
+
+      res.send(response);
+    });
 });
-// axios({
-//   method: "get",
-//   url: "https://www.freeforexapi.com/api/live?pairs=USDZAR",
-//   mode: "no-cors",
-//   withCredentials: true,
-//   crossdomain: true,
-//   headers: {
-//     "Access-Control-Allow-Origin": "*",
-//     "Content-Type": "application/json",
-//   },
-//   // credentials: "same-origin",
-// }).then((res) => {
-//   console.log(res);
-// });
-axios({
-  method: "get",
-  url: "https://api.mybitx.com/api/1/ticker?pair=XBTZAR",
-  mode: "no-cors",
-  withCredentials: true,
-  crossdomain: true,
-  headers: {
-    "Access-Control-Allow-Origin": "*",
-    "Content-Type": "application/json",
-  },
-  // credentials: "same-origin",
-}).then((res) => {
-  console.log(res.data);
-});
+
+app.get("");
+
+app.listen("8080");
